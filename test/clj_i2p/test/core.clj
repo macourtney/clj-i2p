ee8c727444b92f49c7ec30b6ab7a63a53ef7d4e3
Matt Courtney <macourtney@gmail.com>
2012-07-18 16:28:17 -0400

    Added tests for the mock support in core.

--- test/clj_i2p/test/core.clj
+++ test/clj_i2p/test/core.clj
@@ -2,5 +2,22 @@
   (:use [clj-i2p.core])
   (:use [clojure.test]))
 
+(def test-destination "alskn09jh0pksd9-09j")
+(def test-request-data { :test "foo" })
+(def test-response-data { :test "bar" })
+
 (deftest current-destination-test
   (is (nil? (current-destination))))
+
+(defn mock-network-fn [destination data]
+  (is (= destination test-destination))
+  (is (= data test-request-data))
+  test-response-data)
+
+(deftest test-mock-network
+  (is (nil? @mock-network))
+  (set-mock-network mock-network-fn)
+  (is (= @mock-network mock-network-fn))
+  (is (= (send-message test-destination test-request-data) test-response-data))
+  (clear-mock-network)
+  (is (nil? @mock-network)))
