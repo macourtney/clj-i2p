b9e8eec87dde26e281d83e5ff420c25436bc8ea8
Matthew Courtney <macourtney@gmail.com>
2012-11-21 13:29:44 -0500

    Removed clojure reference and updated .gitignore for lein 2.

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :dependencies [[org.clojars.macourtney/i2p "0.8.7-0"]
                  [org.clojars.macourtney/mstreaming "0.8.7-0"]
                  [org.clojars.macourtney/streaming "0.8.7-0"]
-                 [org.clojure/clojure "1.2.1"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.clojure/tools.logging "0.2.0"]]
-  :dev-dependencies [[org.clojure/clojure "1.2.1"]])
+                 [org.clojure/tools.logging "0.2.0"]])
