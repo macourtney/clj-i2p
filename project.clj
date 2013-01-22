61271958cbe69378e5c78a4a99f7d6520b4aff82
Matt Courtney <macourtney@gmail.com>
2013-01-22 15:17:20 -0500

    Updated to the latest version of I2P and Leiningen.

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject clj-i2p "1.0.0-SNAPSHOT"
   :description "Clj-i2p is a wrapper for an interface to the i2p p2p network."
-  :dependencies [[org.clojars.macourtney/i2p "0.8.7-0"]
-                 [org.clojars.macourtney/mstreaming "0.8.7-0"]
-                 [org.clojars.macourtney/streaming "0.8.7-0"]
-                 [org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojars.macourtney/i2p "0.9.4"]
+                 [org.clojars.macourtney/mstreaming "0.9.4"]
+                 [org.clojars.macourtney/streaming "0.9.4"]
+                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.clojure/tools.logging "0.2.0"]]
-  :dev-dependencies [[org.clojure/clojure "1.2.1"]])
+                 [org.clojure/tools.logging "0.2.0"]])
