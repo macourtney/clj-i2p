08c33eb6569508f51b8c1cb497ff2af7cffe9e49
Matt Courtney <macourtney@gmail.com>
2014-06-15 19:43:16 -0700

    Updated the version 1.0.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-i2p "1.0.0"
+(defproject clj-i2p "1.0.1-SNAPSHOT"
   :description "Clj-i2p is a wrapper for an interface to the i2p p2p network."
   :dependencies [[org.clojars.macourtney/i2p "0.9.13-0"]
                  [org.clojars.macourtney/mstreaming "0.9.13-0"]
