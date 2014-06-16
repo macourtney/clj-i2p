cf13fa73b3b3857ff56446c8af070656a33314c7
Matt Courtney <macourtney@gmail.com>
2014-06-15 19:06:50 -0700

    Updated all of the i2p jars to the latest version.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-i2p "1.0.0-SNAPSHOT"
   :description "Clj-i2p is a wrapper for an interface to the i2p p2p network."
-  :dependencies [[org.clojars.macourtney/i2p "0.9.4"]
-                 [org.clojars.macourtney/mstreaming "0.9.4"]
-                 [org.clojars.macourtney/streaming "0.9.4"]
+  :dependencies [[org.clojars.macourtney/i2p "0.9.13-0"]
+                 [org.clojars.macourtney/mstreaming "0.9.13-0"]
+                 [org.clojars.macourtney/streaming "0.9.13-0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojure/tools.logging "0.2.0"]])
